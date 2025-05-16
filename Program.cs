821fcb2613eaa191d208f81c981d0a5291db8d71
Ntt <Ntt@DESKTOP-UJM0IJC>
2025-05-16 15:22:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2;
+
+namespace OnlineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Zdravic zdravic = new Zdravic();
+            zdravic.Pozdrav();
+            Console.ReadKey();
+        }
+    }
+}
